1ed6a05ae786b8c589b7bb650cc429c69f81c5fc
gaoxingjia <gxj@2871992911qq.com>
2022-08-20 10:32:35 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/Git.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author shkstart
+ * @create 2022-08-20 10:22
+ */
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
