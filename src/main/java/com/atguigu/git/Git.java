b84b49b1e2d97dff3f1b67f8d00490c0c9c286ae
gaoxingjia <gxj@2871992911qq.com>
2022-08-20 14:54:06 +0800

    gitee commit

--- src/main/java/com/atguigu/git/Git.java
+++ src/main/java/com/atguigu/git/Git.java
@@ -13,6 +13,6 @@ public class Git {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("push test");
-            System.out.println("pull test");
+        System.out.println("gitee test");
     }
 }
